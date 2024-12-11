cf3ec817ddc02483f1107dd5cfbc672a09b227f8
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2024-12-11 13:58:34 -0400

    add new games

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Game:
     def __init__(self, name, type, platform):
@@ -8,12 +8,29 @@ class Game:
 
 app = Flask(__name__)
 
+game1 = Game("Mario Wonder", "sidescroller", "Switch")
+game2 = Game("Portal", "Puzzle", "PC")
+games_list = [game1, game2]
+
+@app.route("/create", methods=["POST",])
+def create():
+    nome = request.form["nome"]
+    categoria = request.form["categoria"]
+    console = request.form["console"]
+
+    game = Game(nome, categoria, console)
+
+    games_list.append(game)
+
+    return render_template("list.html", title_var="Games", games=games_list)
+
+@app.route("/new")
+def new_game():
+    return render_template("new.html", titulo="New Game")
 @app.route("/")
 def hello():
 
-    game1 = Game("Mario Wonder", "sidescroller", "Switch")
-    game2 = Game("Portal", "Puzzle", "PC")
-    games_list = [game1, game2]
+
     return render_template("list.html", title_var="test", games=games_list)
 
 app.run()
