27af4f69fdb5b0432c6cc80f18e604fd3d123b19
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2024-12-11 13:24:40 -0400

    dynamic page

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
 
+class Game:
+    def __init__(self, name, type, platform):
+        self.name=name
+        self.type=type
+        self.platform=platform
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("list.html")
+
+    game1 = Game("Mario Wonder", "sidescroller", "Switch")
+    game2 = Game("Portal", "Puzzle", "PC")
+    games_list = [game1, game2]
+    return render_template("list.html", title_var="test", games=games_list)
 
 app.run()
