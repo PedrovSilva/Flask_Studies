e9c98c44c54d077539fa149413adb680fea031d2
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2024-12-11 15:40:13 -0400

    basic authentication

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 
 class Game:
@@ -8,6 +8,8 @@ class Game:
         self.platform=platform
 
 app = Flask(__name__)
+
+app.secret_key = "alura"
 
 game1 = Game("Mario Wonder", "sidescroller", "Switch")
 game2 = Game("Portal", "Puzzle", "PC")
@@ -27,11 +29,34 @@ def create():
 
 @app.route("/new")
 def new_game():
+    if "logged_user" not in session or session["logged_user"] == None:
+        return redirect("/login")
     return render_template("new.html", title_var="New Game")
 @app.route("/")
 def index():
 
 
     return render_template("list.html", title_var="Games", games=games_list)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/autenticar", methods=["POST",])
+def autenticar():
+    if "cake" == request.form["senha"]:
+        session["logged_user"] = request.form["usuario"]
+        flash("User logged!")
+        return redirect("/")
+    else:
+        flash("Wrong password!")
+        return redirect("/login")
+
+@app.route("/logout")
+def logout():
+    session["logged_user"] = None
+    flash("Logout!")
+    return redirect("/")
 
 app.run(debug=True)
